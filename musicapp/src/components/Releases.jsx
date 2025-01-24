269e912a9f993d5b2d439ee5eaf2cbdb42a320bf
Samuele Converso <samu.converso@gmail.com>
2025-01-24 11:20:51 +0000

    Jan 24, 2025, 12:20 PM

--- musicapp/src/components/Releases.jsx
+++ musicapp/src/components/Releases.jsx
@@ -3,6 +3,24 @@ import Song from "./Song";
 import { useEffect, useState } from "react";
 
 function Releases() {
+  const [songs, setSongs] = useState();
+
+  const URL =
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=lady%20gaga";
+
+  const getSongs = async () => {
+    try {
+      const response = await fetch(URL);
+      if (response.ok) {
+        const data = await response.json();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <div className="container">
       <div className="text-white mt-3">
