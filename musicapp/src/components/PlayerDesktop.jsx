61f1baa3d9a4345de07bc96f48c545c42cf05122
Samuele Converso <samu.converso@gmail.com>
2025-01-24 13:45:42 +0000

    Jan 24, 2025, 2:45 PM

--- musicapp/src/components/PlayerDesktop.jsx
+++ musicapp/src/components/PlayerDesktop.jsx
@@ -1,5 +1,23 @@
+import * as Icon from "react-bootstrap-icons";
+
 function PlayerDesktop() {
-  return <div className="container-fluid"></div>;
+  return (
+    <div
+      className="container-fluid d-none d-lg-block"
+      style={{ backgroundColor: "#2C2C2C", position: "fixed", top: "0" }}
+    >
+      <div className="row">
+        <div className="col-4">
+          <Icon.PlayFill style={{ color: "gray", fontSize: "22px" }} />
+          <Icon.FastForwardFill
+            style={{ color: "gray", marginLeft: "10px", fontSize: "22px" }}
+          />
+        </div>
+        <div className="col-4"></div>
+        <div className="col-4"></div>
+      </div>
+    </div>
+  );
 }
 
 export default PlayerDesktop;
