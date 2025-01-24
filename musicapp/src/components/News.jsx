7215d83a39b090d468617f4dc2cbcff041fc7aa4
Samuele Converso <samu.converso@gmail.com>
2025-01-24 10:33:15 +0000

    Jan 24, 2025, 11:33 AM

--- musicapp/src/components/News.jsx
+++ musicapp/src/components/News.jsx
@@ -1,43 +1,42 @@
 function News() {
   return (
-    <div>
+    <>
       <div className="container mt-3">
         <div>
-          <p className="h1 text-white">Novità</p>
-          <hr className="text-white" />
-        </div>
+          <div>
+            <p className="h1 text-white">Novità</p>
+            <hr className="text-white" />
+          </div>
 
-        <div className="container-fluid text-white">
-          <div className="row">
-            <div className="col-6">
-              <p style={{ fontSize: "10px" }}>NUOVA STAZIONE RADIO</p>
-              <p style={{ fontSize: "12px" }}>
-                Rilassati, al resto pensiamo noi. <br /> Ascolta Apple Muisc
-                Chill
-              </p>
-              <img
-                src="../../public/assets/img/1a.png"
-                width="100%"
-                style={{ borderRadius: "10px" }}
-              />
-            </div>
-            <div className="col-6">
-              <p style={{ fontSize: "10px" }}>NUOVA STAZIONE RADIO</p>
-              <p style={{ fontSize: "12px" }}>
-                Ecco la nuova casa
-                <br />
-                della musica latina
-              </p>
-              <img
-                src="../../public/assets/img/1b.png"
-                width="100%"
-                style={{ borderRadius: "10px" }}
-              />
+          <div className="container-fluid text-white">
+            <div className="row">
+              <div className="col-6">
+                <p style={{ fontSize: "10px" }}>NUOVA STAZIONE RADIO</p>
+                <p style={{ fontSize: "12px" }}>
+                  Rilassati, al resto pensiamo noi. Ascolta Apple Muisc Chill
+                </p>
+                <img
+                  src="../../public/assets/img/1a.png"
+                  width="100%"
+                  style={{ borderRadius: "10px" }}
+                />
+              </div>
+              <div className="col-6">
+                <p style={{ fontSize: "10px" }}>NUOVA STAZIONE RADIO</p>
+                <p style={{ fontSize: "12px" }}>
+                  Ecco la nuova casa della musica latina
+                </p>
+                <img
+                  src="../../public/assets/img/1b.png"
+                  width="100%"
+                  style={{ borderRadius: "10px" }}
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
