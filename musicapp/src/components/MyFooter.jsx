c86216673f5b06aa3a888c9c6c63fb4df76247ef
Samuele Converso <samu.converso@gmail.com>
2025-01-24 12:50:13 +0000

    Jan 24, 2025, 1:50 PM

--- musicapp/src/components/MyFooter.jsx
+++ musicapp/src/components/MyFooter.jsx
@@ -4,9 +4,15 @@ function MyFooter() {
       className="container-fluid text-white mt-3"
       style={{ backgroundColor: "#323232" }}
     >
-      <p className="m-0">Italia</p>
-      <p className="m-0">|</p>
-      <p className="m-0">English (UK)</p>
+      <div className="d-flex gap-3 mb-3 pt-3">
+        <p className="m-0">Italia</p>
+        <p className="m-0">|</p>
+        <p className="m-0">English (UK)</p>
+      </div>
+
+      <p className="m-0">
+        Copyright &copy; 2024 Apple Inc. Tutti i diritti riservati.
+      </p>
     </div>
   );
 }
