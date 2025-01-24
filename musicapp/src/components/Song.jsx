b653037773ac4703f6489d10fdf2b53cf7e1ad72
Samuele Converso <samu.converso@gmail.com>
2025-01-24 11:49:27 +0000

    Jan 24, 2025, 12:49 PM

--- musicapp/src/components/Song.jsx
+++ musicapp/src/components/Song.jsx
@@ -5,7 +5,10 @@ function Song(props) {
   return (
     <>
       <Col>
-        <img src={props.song.md5_image} />
+        <img
+          src={props.song.album.cover}
+          style={{ borderRadius: "10px", width: "100%" }}
+        />
         <div className="text-white">
           <p>{props.song.title}</p>
           <p>{props.song.artist.name}</p>
