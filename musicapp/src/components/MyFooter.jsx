b21f0760e67767cbf081e527ee45996a156a1d75
Samuele Converso <samu.converso@gmail.com>
2025-01-24 12:47:22 +0000

    Jan 24, 2025, 1:47 PM

--- musicapp/src/components/MyFooter.jsx
+++ musicapp/src/components/MyFooter.jsx
@@ -1,10 +1,12 @@
 function MyFooter() {
   return (
     <div
-      className="container text-white"
+      className="container-fluid text-white mt-3"
       style={{ backgroundColor: "#323232" }}
     >
-      <p>Italia</p>
+      <p className="m-0">Italia</p>
+      <p className="m-0">|</p>
+      <p className="m-0">English (UK)</p>
     </div>
   );
 }
