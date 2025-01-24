15a309f70307f7a9fdbd30e535c04f2724a792c6
Samuele Converso <samu.converso@gmail.com>
2025-01-24 11:05:17 +0000

    Jan 24, 2025, 12:05 PM

---
+++ musicapp/src/components/Releases.jsx
@@ -0,0 +1,16 @@
+import * as Icon from "react-bootstrap-icons";
+
+function Releases() {
+  return (
+    <>
+      <div>
+        <p>
+          Nuove uscite
+          <Icon.ArrowRight className="ms-2" />
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Releases;
