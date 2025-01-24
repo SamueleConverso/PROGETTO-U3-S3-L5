42b54639e6632559a750cec76c275859d6e402bf
Samuele Converso <samu.converso@gmail.com>
2025-01-24 10:26:52 +0000

    Jan 24, 2025, 11:26 AM

--- musicapp/src/components/News.jsx
+++ musicapp/src/components/News.jsx
@@ -10,13 +10,29 @@ function News() {
         <div className="container-fluid text-white">
           <div className="row">
             <div className="col-6">
-              <p style={{ fontSize: "12px" }}>NUOVA STAZIONE RADIO</p>
+              <p style={{ fontSize: "10px" }}>NUOVA STAZIONE RADIO</p>
               <p style={{ fontSize: "12px" }}>
                 Rilassati, al resto pensiamo noi. <br /> Ascolta Apple Muisc
                 Chill
               </p>
+              <img
+                src="../../public/assets/img/1a.png"
+                width="100%"
+                style={{ borderRadius: "10px" }}
+              />
             </div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <p style={{ fontSize: "10px" }}>NUOVA STAZIONE RADIO</p>
+              <p style={{ fontSize: "12px" }}>
+                Rilassati, al resto pensiamo noi. <br /> Ascolta Apple Muisc
+                Chill
+              </p>
+              <img
+                src="../../public/assets/img/1b.png"
+                width="100%"
+                style={{ borderRadius: "10px" }}
+              />
+            </div>
           </div>
         </div>
       </div>
