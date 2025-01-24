4295c7513228130a9436fc7c71cfb87d393c91fc
Samuele Converso <samu.converso@gmail.com>
2025-01-24 09:44:31 +0000

    Jan 24, 2025, 10:44 AM

--- musicapp/src/components/MyNavbarLg.jsx
+++ musicapp/src/components/MyNavbarLg.jsx
@@ -42,7 +42,7 @@ function MyNavbarLg() {
     //   <p className="text-danger mb-0">Accedi</p>
     // </Navbar>
     <>
-      <div style={{ "background-color": "#252526" }}>
+      <div style={{ "background-color": "#252526", "" }}>
         <p className="text-white mb-0 d-flex">
           <img
             src="../../public/assets/img/apple.svg"
