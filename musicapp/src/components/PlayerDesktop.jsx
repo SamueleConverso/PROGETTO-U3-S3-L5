594032707f20c9d14d892d55c8eb7fd44e4547e8
Samuele Converso <samu.converso@gmail.com>
2025-01-24 13:50:13 +0000

    Jan 24, 2025, 2:50 PM

--- musicapp/src/components/PlayerDesktop.jsx
+++ musicapp/src/components/PlayerDesktop.jsx
@@ -7,9 +7,18 @@ function PlayerDesktop() {
       style={{ backgroundColor: "#2C2C2C", position: "fixed", top: "0" }}
     >
       <div className="row">
-        <div className="col-4">
+        <div className="col-4 d-flex gap-2">
+          <Icon.Shuffle
+            className="align-self-center"
+            style={{ color: "gray", fontSize: "12px" }}
+          />
+          <Icon.SkipBackwardFill style={{ color: "gray", fontSize: "22px" }} />
           <Icon.PlayFill style={{ color: "gray", fontSize: "22px" }} />
           <Icon.FastForwardFill style={{ color: "gray", fontSize: "22px" }} />
+          <Icon.Repeat
+            className="align-self-center"
+            style={{ color: "gray", fontSize: "12px" }}
+          />
         </div>
         <div className="col-4"></div>
         <div className="col-4"></div>
