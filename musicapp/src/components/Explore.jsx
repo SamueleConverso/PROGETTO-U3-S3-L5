7604ca8ab43189ef3bc32097c338860d867061a7
Samuele Converso <samu.converso@gmail.com>
2025-01-24 12:07:27 +0000

    Jan 24, 2025, 1:07 PM

--- musicapp/src/components/Explore.jsx
+++ musicapp/src/components/Explore.jsx
@@ -2,19 +2,24 @@ function Explore() {
   return (
     <div className="container">
       <h2 className="text-white">Altro da esplorare</h2>
-      <div
-        style={{
-          backgroundColor: "#2A2A2A",
-          width: "fit-content",
-          borderRadius: "10px",
-          padding: "10px",
-        }}
-      >
-        <div className="d-flex">
-          <p className="text-danger m-0">
-            Esplora per genere&nbsp;&nbsp;&nbsp;
-          </p>
-          <p className="text-danger m-0">{">"}</p>
+      <div className="row">
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
