19a87ad8f074500b6fe7b45a0e462fc65f9a7e4f
Samuele Converso <samu.converso@gmail.com>
2025-01-24 12:10:09 +0000

    Jan 24, 2025, 1:10 PM

--- musicapp/src/components/Explore.jsx
+++ musicapp/src/components/Explore.jsx
@@ -21,6 +21,158 @@ function Explore() {
             </div>
           </div>
         </div>
+
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div
+            style={{
+              backgroundColor: "#2A2A2A",
+              borderRadius: "10px",
+              padding: "10px 20px",
+            }}
+          >
+            <div className="d-flex justify-content-between w-100">
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                Esplora per genere&nbsp;&nbsp;&nbsp;
+              </p>
+              <p className="text-danger m-0" style={{ fontSize: "20px" }}>
+                {">"}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
